37bb75c7cd3dbab99f67a44309e00b751f2dc607
Swapnil Sharma <swapnilsharma1163@gmail.com>
2026-04-23 02:07:41 +0530

    Initial commit

---
+++ src/main/java/org/example/multiagentorchestration/MultiAgentOrchestrationApplication.java
@@ -0,0 +1,13 @@
+package org.example.multiagentorchestration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiAgentOrchestrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiAgentOrchestrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/multiagentorchestration/MultiAgentOrchestrationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.multiagentorchestration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiAgentOrchestrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
